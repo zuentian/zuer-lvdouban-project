d62cd51546529b51b8d83c5988548dfd246422af
zuer <1974900537@example.com>
2020-03-30 16:46:39 +0800

    爬虫系统

---
+++ src/views/crawler/dbMovieInfo/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+
+        <el-button  type="primary" v-waves icon="el-icon-search" class="filter-item" @click="get()">搜索</el-button>
+
+
+    </div>
+</template>
+<script>
+import {getDbMovieInfo} from 'api/crawler/index.js'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        get(){
+            getDbMovieInfo().then(res=>{
+
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
